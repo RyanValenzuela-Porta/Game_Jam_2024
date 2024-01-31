85ec1b53e3e0bcc92a03c2214d483d4d0d426b92
Jackie <zjackiewang03@gmail.com>
2024-01-31 17:07:29 +0000

    stand and walk animation, but when pressing diagonal moves, it creates two dinos

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -12,6 +12,7 @@ public class Player {
 	// animation
 	private static final int cols = 9, rows = 1;
 	Animation<TextureRegion> walkAnimation;
+	Animation<TextureRegion> standAnimation;
 	Texture walkSheet;
 	float stateTime;
 
@@ -19,7 +20,7 @@ public class Player {
 	private Texture img;
 	private float playerX = 505;
 	private float playerY = 327;
-	private float speed = 500;
+	private float speed = 200;
 	private int hp;
 	private float regen;
 	private float dmg;
@@ -36,22 +37,28 @@ public class Player {
 		// batch.draw(img, 0, 0,0,0,100,100);
 		stateTime += Gdx.graphics.getDeltaTime();
 		TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime,true);
-		batch.draw(currentFrame, playerX,playerY);
+		TextureRegion currentFrame2 = standAnimation.getKeyFrame(stateTime,true); 
 		int maxY = 655;
 		int minY = 55;
 		int maxX = 1205;
 		int minX = 50;
-		System.out.println(playerY);
+		if(!Gdx.input.isKeyPressed(Input.Keys.W) && !Gdx.input.isKeyPressed(Input.Keys.S) && !Gdx.input.isKeyPressed(Input.Keys.A) && !Gdx.input.isKeyPressed(Input.Keys.D)){
+			batch.draw(currentFrame2, playerX,playerY);
+		}
 		if (Gdx.input.isKeyPressed(Input.Keys.W) && playerY < maxY) {
+			batch.draw(currentFrame, playerX, playerY);
 			playerY += (Gdx.graphics.getDeltaTime() * speed);
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.S) && playerY > minY) {
+			batch.draw(currentFrame, playerX, playerY);
 			playerY -= (Gdx.graphics.getDeltaTime() * speed);
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.A) && playerX > minX) {
+			batch.draw(currentFrame, playerX, playerY);
 			playerX -= (Gdx.graphics.getDeltaTime() * speed);
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.D) && playerX < maxX) {
+			batch.draw(currentFrame, playerX, playerY);
 			playerX += (Gdx.graphics.getDeltaTime() * speed);
 		}
 	}
@@ -75,8 +82,12 @@ public class Player {
                 walkFrames[index++] = tmp[i][j];
             }
         }
+		TextureRegion[] standFrames = new TextureRegion[2];
+		standFrames[0] = tmp[0][0];
+		standFrames[1] = tmp[0][1];
 
         walkAnimation = new Animation<TextureRegion>(0.11f, walkFrames);
+		standAnimation = new Animation<TextureRegion>(0.5f, standFrames);
         stateTime = 0f;
 	}
 
