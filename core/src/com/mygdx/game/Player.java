d83e9870e2ba0729ab5b4e7aec0f4b433cfe1f0d
Ryan Valenzuela-Porta <ryanpogibags30@gmail.com>
2024-01-31 17:13:37 +0000

    Flip the dino when facing left

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -23,7 +23,9 @@ public class Player {
 	private int hp;
 	private float regen;
 	private float dmg;
-
+	private boolean facingRight = true;
+	private float width = 16;
+	private float height = 16;
 	public Player(SpriteBatch newBatch, Texture newImg) {
 		batch = newBatch;
 		img = newImg;
@@ -36,7 +38,9 @@ public class Player {
 		// batch.draw(img, 0, 0,0,0,100,100);
 		stateTime += Gdx.graphics.getDeltaTime();
 		TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime,true);
-		batch.draw(currentFrame, playerX,playerY);
+		//If the player is not facing right, draw the player with negative width to flip them.
+		batch.draw(currentFrame, !facingRight ? playerX+width : playerX,playerY,!facingRight ? -width:width,height);
+		
 		int maxY = 655;
 		int minY = 55;
 		int maxX = 1205;
@@ -50,9 +54,11 @@ public class Player {
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.A) && playerX > minX) {
 			playerX -= (Gdx.graphics.getDeltaTime() * speed);
+			facingRight=false;
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.D) && playerX < maxX) {
 			playerX += (Gdx.graphics.getDeltaTime() * speed);
+			facingRight=true;
 		}
 	}
 
