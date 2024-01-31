5e4d494802166d90752c472f950af9d7b9ffde3a
parth13075 <109304629+parth13075@users.noreply.github.com>
2024-01-31 17:08:48 +0000

    first-commit

---
+++ core/src/com/mygdx/game/Enemy.java
@@ -0,0 +1,50 @@
+package com.mygdx.game;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.*;
+
+public class Enemy {
+    private static final int cols = 9, rows = 1;
+    private SpriteBatch batch;
+    private Texture img;
+    Animation<TextureRegion> walkAnimation;
+	Texture walkSheet;
+	float stateTime;
+
+    public Enemy(SpriteBatch newBatch, Texture newImg) {
+		batch = newBatch;
+		img = newImg;
+	}
+
+    public void draw(){
+        img = new Texture("tileset.png");
+        stateTime += Gdx.graphics.getDeltaTime();
+		TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime,true);
+    }
+
+    public void createIdleAnimation(){
+        walkSheet = new Texture(Gdx.files.internal("enemyWalksheet.png"));
+
+        TextureRegion[][] tmp = TextureRegion.split(walkSheet,    //Splits walksheet into individual frames
+                                    walkSheet.getWidth()/ cols,
+                                    walkSheet.getHeight()/ rows);
+
+        TextureRegion[] walkFrames = new TextureRegion[cols * rows];
+        int index = 0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                walkFrames[index++] = tmp[i][j];
+            }
+        }
+
+        walkAnimation = new Animation<TextureRegion>(0.11f, walkFrames);
+        stateTime = 0f;
+    }
+
+    public void dispose() {
+		img.dispose();
+		walkSheet.dispose();
+	}
+}
